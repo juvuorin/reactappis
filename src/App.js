f6350a7c18c561778af9b8b20a7a11341a51159f
Juuso Vuorinen <juvuorin@gmail.com>
2021-10-15 15:34:46 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  //const muuttuja="Moro2"
+  //let muuttuja = 0 
+  const päivät = ["ma","ti","ke","to","pe","la","su"] 
+  const [valittuPäivä, setValittuPäivä] = useState(päivät[0])
+  console.log("App funktiota kutsuttiin")
+
+  const painike1Painettu = () =>{
+    setValittuPäivä(päivät[0])
+  }
+  const painike2Painettu = () =>{
+    setValittuPäivä(päivät[1])
+    
+  }
+  const painike3Painettu = () =>{
+    setValittuPäivä(päivät[2])
+    
+  }
+  const painike4Painettu = () =>{
+    setValittuPäivä(päivät[3])
+    
+  }
+  const painike5Painettu = () =>{
+    setValittuPäivä(päivät[4])
+    
+  }
+  const painike6Painettu = () =>{
+    setValittuPäivä(päivät[5])
+    
+  }
+  const painike7Painettu = () =>{
+    setValittuPäivä(päivät[6])
+    
+  }
+
+//JSX  map EI for of 
+return (<div>
+  {päivät.map(item=><p>{item}</p>)}  
+  </div>
+
   );
 }
 

8ec28cedcf16920ca5cd899098d36d7a1eeeabc5
Juuso Vuorinen <juvuorin@gmail.com>
2021-10-18 10:52:04 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -5,46 +5,38 @@ import React, { useState } from 'react';
 
 function App() {
 
-  //const muuttuja="Moro2"
-  //let muuttuja = 0 
-  const päivät = ["ma","ti","ke","to","pe","la","su"] 
-  const [valittuPäivä, setValittuPäivä] = useState(päivät[0])
-  console.log("App funktiota kutsuttiin")
+  const [ylempiLista,setYlempiLista] = useState(["Paavo", "Tytti","Ilkka","Sauli"])
+  const [alempiLista,setAlempiLista] = useState([])
 
-  const painike1Painettu = () =>{
-    setValittuPäivä(päivät[0])
-  }
-  const painike2Painettu = () =>{
-    setValittuPäivä(päivät[1])
+  const alasPainettu = () =>{
+    let ylemmanViimeinenAlkio = ylempiLista[ylempiLista.length-1]
+    setAlempiLista([...alempiLista,ylemmanViimeinenAlkio])
+    ylempiLista.pop()
+    setYlempiLista([...ylempiLista])
     
+   
   }
-  const painike3Painettu = () =>{
-    setValittuPäivä(päivät[2])
-    
-  }
-  const painike4Painettu = () =>{
-    setValittuPäivä(päivät[3])
-    
-  }
-  const painike5Painettu = () =>{
-    setValittuPäivä(päivät[4])
-    
-  }
-  const painike6Painettu = () =>{
-    setValittuPäivä(päivät[5])
-    
-  }
-  const painike7Painettu = () =>{
-    setValittuPäivä(päivät[6])
-    
+  const ylösPainettu = () =>{
+
+    let alemmanEkaAlkio = alempiLista[0]
+    setYlempiLista([...ylempiLista, alemmanEkaAlkio])
+    alempiLista.shift()
+    setAlempiLista([...alempiLista])
+      
   }
 
-//JSX  map EI for of 
-return (<div>
-  {päivät.map(item=><p>{item}</p>)}  
+  return (<div>
+    {ylempiLista.map(item=><div>{item}</div>)}
+    <button onClick={ylösPainettu}   >ylös</button>
+    <button onClick={alasPainettu}>alas</button>
+    {alempiLista.map(item=><div>{item}</div>)}
+
+
   </div>
 
-  );
+
+  )
+
 }
 
 export default App;

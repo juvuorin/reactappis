0986c6daa64e8f9da3073fed6a6ed9e9e6e709ed
Juuso Vuorinen <juvuorin@gmail.com>
2021-10-25 13:03:08 +0300

    tuotteiden haku tuoteryhmien perusteella

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ function App() {
 
   const [kategoriat, setKategoriat] = useState([])
   const [ladattu, setLadattu] = useState(false)
+  const [kategoria, setKategoria] =useState(0)
+  const [items, setItems] =useState([])
+
+//  https://api.huuto.net/1.1/items?category=86
 
 //  const [count, setCount] = useState(0);
   //https://api.huuto.net/1.1/categories
@@ -26,6 +30,7 @@ function App() {
     axios.get('https://api.huuto.net/1.1/categories')
       .then(function (response) {
         // handle success
+        console.log(response.data.categories)
         setKategoriat(response.data.categories);
         setLadattu(true)
 
@@ -43,11 +48,46 @@ function App() {
     console.log("kutsuttiin")
   }, []);
 
+  useEffect(() => {
 
+    /* async
+    try {
+
+    } catch {}
+    finally{} */
+
+    axios.get('https://api.huuto.net/1.1/items?category='+kategoria)
+      .then(function (response) {
+        // handle success
+        console.log(response.data.items)
+        setItems(response.data.items)
+       // setKategoriat(response.data.categories);
+       // setLadattu(true)
+
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        console.log("Tämö suoritetaan aina")
+        // always executed
+      });
+
+
+    console.log("kutsuttiin")
+  }, [kategoria]);
+
+  const kategoriaPainettu=(id)=>{
+    setKategoria(id)
+    console.log(id)
+  }
+    
   return (
     <div>
-      {!ladattu?"Dataa lataillaan....":kategoriat.map(kategoria=><div key={kategoria.id}>{kategoria.title}</div>)}
+      {!ladattu?"Dataa lataillaan....":kategoriat.map(kategoria=><button onClick={()=>kategoriaPainettu(kategoria.id)} key={kategoria.id}>{kategoria.title}</button>)}
       
+      <div>{items.map(item=><div>{item.title}</div>)}</div>
     </div>
   );
 

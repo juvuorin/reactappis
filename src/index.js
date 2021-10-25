6f172b2e5cdb68b46aae4acd62562fb2b92e7f9f
Juuso Vuorinen <juvuorin@gmail.com>
2021-10-25 14:58:50 +0300

    ajastin lisätty

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Timer from './Timer'
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Timer />
   </React.StrictMode>,
   document.getElementById('root')
 );

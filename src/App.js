69292b6a176261904bcd52f8efe2d94054cd853f
Juuso Vuorinen <juvuorin@gmail.com>
2021-10-25 12:20:01 +0300

    huuto net kategoriaoiden listaus

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 //import logo from './logo.svg';
 //import './App.css';
 import Box from '@mui/material/Box';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Nappula from './Nappula'
 import SimpleDialogDemo from './SimpleDialog'
 import App3 from './App3'
-
+const axios = require('axios');
 function App() {
 
-  return (<div><App3/></div>)
+
+  const [kategoriat, setKategoriat] = useState([])
+  const [ladattu, setLadattu] = useState(false)
+
+//  const [count, setCount] = useState(0);
+  //https://api.huuto.net/1.1/categories
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+
+    /* async
+    try {
+
+    } catch {}
+    finally{} */
+
+    axios.get('https://api.huuto.net/1.1/categories')
+      .then(function (response) {
+        // handle success
+        setKategoriat(response.data.categories);
+        setLadattu(true)
+
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        console.log("Tämö suoritetaan aina")
+        // always executed
+      });
+
+
+    console.log("kutsuttiin")
+  }, []);
+
+
+  return (
+    <div>
+      {!ladattu?"Dataa lataillaan....":kategoriat.map(kategoria=><div key={kategoria.id}>{kategoria.title}</div>)}
+      
+    </div>
+  );
 
 }
 

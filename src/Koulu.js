c6ff8a1a6d2ee53836bb4281e1b1b4767c178199
Juuso Vuorinen <juvuorin@gmail.com>
2021-11-08 15:32:26 +0200

    lisättiin useEffect

--- src/Koulu.js
+++ src/Koulu.js
@@ -1,37 +1,53 @@
 //import logo from './logo.svg';
 //import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 //Tentti1
 import Luokka from './Luokka';
 
-let oppilas1 = { nimi: "Otto Onninpoika", osaalukea:true }
-let oppilas2 = { nimi: "Pekka Perä",osaalukea:false }
-let oppilas3 = { nimi: "Tarmo Tiusanen",osaalukea:true }
+let oppilas1 = { nimi: "Otto Onninpoika", osaalukea: true }
+let oppilas2 = { nimi: "Pekka Perä", osaalukea: false }
+let oppilas3 = { nimi: "Tarmo Tiusanen", osaalukea: true }
 
 let luokka1 = { kirjain: "A", oppilaat: [oppilas1, oppilas2, oppilas3] }
 
 
-let oppilas4 = { nimi: "Raimo Riitala" ,osaalukea:true}
-let oppilas5 = { nimi: "Kirski Kokkonen",osaalukea:false }
-let oppilas6 = { nimi: "Fabian Fedorov" ,osaalukea:true}
-let oppilas7 = { nimi: "Daniel Downing",osaalukea:true }
+let oppilas4 = { nimi: "Raimo Riitala", osaalukea: true }
+let oppilas5 = { nimi: "Kirski Kokkonen", osaalukea: false }
+let oppilas6 = { nimi: "Fabian Fedorov", osaalukea: true }
+let oppilas7 = { nimi: "Daniel Downing", osaalukea: true }
 
 let luokka2 = { kirjain: "B", oppilaat: [oppilas4, oppilas5, oppilas6, oppilas7] }
 
 var koulu_ = { nimi: "alakoulu", luokat: [luokka1, luokka2] }
 
 
-function Koulu() {
-
-    const [koulu, setKoulu] = useState(koulu_)
+const Koulu=()=> {
+    const [dataNoudettu, setDataNoudettu] = useState(false)
+    const [koulu, setKoulu] = useState()
     //    [koulu, setKoulu] = useState(koulu)
     console.log("Koulu renderöitiin")
-//REDUX
+    //REDUX
+
+    useEffect(() => {
+
+        if (!dataNoudettu) {
+            let result = localStorage.getItem("kouluData")
+            if (result == null) {
+                setKoulu(koulu_)
+            } else {
+                setKoulu(JSON.parse(result))
+            }
+            setDataNoudettu(true)
+        } else {
+            localStorage.setItem("kouluData", JSON.stringify(koulu))
+        }
+
+    }, [koulu])
     const dispatch = (o) => {
         //  o = {type:"POISTA_OPPILAS",data: {oppilasIndex:0, luokkaIndex:0}}
         let kouluKopio = (JSON.parse(JSON.stringify(koulu)))
-       // let kouluKopio = koulu
+        // let kouluKopio = koulu
 
         switch (o.type) {
             case "POISTA_OPPILAS":
@@ -45,17 +61,17 @@ function Koulu() {
                 kouluKopio.luokat.push({ kirjain: "T", oppilaat: [] })
                 break;
             case "OPPILAAN_NIMI_MUUTTUI":
-                kouluKopio.luokat[o.data.luokkaIndex].oppilaat[o.data.oppilasIndex].nimi=o.data.nimi
+                kouluKopio.luokat[o.data.luokkaIndex].oppilaat[o.data.oppilasIndex].nimi = o.data.nimi
                 break;
             case "OPPILAAN_LUKUTAITOTIETO_MUUTTUI":
-                kouluKopio.luokat[o.data.luokkaIndex].oppilaat[o.data.oppilasIndex].osaalukea=!kouluKopio.luokat[o.data.luokkaIndex].oppilaat[o.data.oppilasIndex].osaalukea
+                kouluKopio.luokat[o.data.luokkaIndex].oppilaat[o.data.oppilasIndex].osaalukea = !kouluKopio.luokat[o.data.luokkaIndex].oppilaat[o.data.oppilasIndex].osaalukea
                 break;
             default: throw "Nyt on jokin vialla"
-            
+
 
 
         }
-        console.log(kouluKopio)   
+        console.log(kouluKopio)
         setKoulu(kouluKopio)
 
     }
@@ -83,9 +99,10 @@ function Koulu() {
         setKoulu(kouluKopio)
 
     }
- */    return (<div>{koulu.nimi}{koulu.luokat.map((item, index) => <Luokka luokka={item} luokkaIndex={index} dispatch={dispatch} koulu={koulu} />)}
+ */
+    return (<div>{(dataNoudettu==true) && <div>{koulu.nimi}{koulu.luokat.map((item, index) => <Luokka luokka={item} luokkaIndex={index} dispatch={dispatch} koulu={koulu} />)}
         <button onClick={() => dispatch({ type: "LISÄÄ_LUOKKA" })}>Lisää luokka</button>
 
-    </div>);
+    </div>}</div>);
 }
 export default Koulu;
